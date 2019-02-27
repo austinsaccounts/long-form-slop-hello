aac7197e5c70b9b412d157e6271b3287ee3cfad6
Austin C <austins.accts@gmail.com>
2019-02-27 11:27:19 -0800

    edit text for readability

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 var heading =React.createElement('h1', {}, ' Less than helpful queue');
